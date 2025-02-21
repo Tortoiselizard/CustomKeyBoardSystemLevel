73217e89e40d51255fb28957b4d58a3554075286
Troy Pernía <troy00pernia@gmail.com>
2025-02-20 22:20:24 -0400

    changed file app/src/main/java/com/example/customkeyboardsystemlevel/CustomKeyboardApp: the methods onStartInputView and onWindowHidden were added (line 21-32). the function isServiceRunning was added (line 39-48)

--- app/src/main/java/com/example/customkeyboardsystemlevel/CustomKeyboardApp.kt
+++ app/src/main/java/com/example/customkeyboardsystemlevel/CustomKeyboardApp.kt
@@ -1,34 +1,49 @@
 package com.example.customkeyboardsystemlevel
 
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import android.inputmethodservice.InputMethodService
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.view.inputmethod.InputConnection
 
 class CustomKeyboardApp : InputMethodService() {
 
     override fun onCreateInputView(): View {
+
         val keyboardView = LayoutInflater.from(this).inflate(R.layout.custom_keyboard_layout, null)
-
-        // Configurar eventos de las teclas
-        val keys = listOf(
-            R.id.key_1 to "1", R.id.key_2 to "2", R.id.key_3 to "3", R.id.key_4 to "4", R.id.key_5 to "5",
-            R.id.key_6 to "6", R.id.key_7 to "7", R.id.key_8 to "8", R.id.key_9 to "9", R.id.key_0 to "0",
-            R.id.key_a to "A", R.id.key_b to "B", R.id.key_c to "C"
-        )
-
-        keys.forEach { (id, value) ->
-            keyboardView.findViewById<Button>(id).setOnClickListener {
-                sendKey(value)
-            }
-        }
-
         return keyboardView
+    }
+
+    override fun onStartInputView(editorInfo: EditorInfo?, restarting: Boolean) {
+        // Iniciar la franja flotante
+        if (!isServiceRunning(FloatingBannerService::class.java)) {
+            startService(Intent(this, FloatingBannerService::class.java))
+        }
+    }
+
+    override fun onWindowHidden() {
+        // Detener el servicio cuando el teclado se destruye
+        stopService(Intent(this, FloatingBannerService::class.java))
+        super.onWindowHidden()
     }
 
     private fun sendKey(char: String) {
         val inputConnection: InputConnection? = currentInputConnection
         inputConnection?.commitText(char, 1)
+    }
+
+    // Método para verificar si el servicio está en ejecución
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 }
